2b81e6851bdec1b14ff502d77fb9551a147f6de3
fassbender_gor@K2AC.LOCAL <fassbender_gor@K2AC.LOCAL>
2018-12-13 10:41:23 +0100

    Label works

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,4 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
-public class Canvas {
+public class Canvas extends Application {
+
+	private static final String name = "Canvas";
+	private Label label;
+
+	@Override
+	public void init() {
+		label = new Label("Nigger");
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		StackPane pane = new StackPane();
+		pane.getChildren().add(label);
+		Scene scene = new Scene(pane, 200, 200);
+		stage.setTitle(name);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+	
 
 }
